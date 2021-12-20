f980fbbfef49bb93fab4438da0ec09e32db08bd9
zakieki55 <96403048+zakieki55@users.noreply.github.com>
2021-12-20 10:55:50 +0700

    Add files via upload

---
+++ Java.table.data.act1/src/main/java/UMY/Java/table/data/act1/ktpData.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UMY.Java.table.data.act1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author muhammad dzaki
+ */
+public class ktpData {
+    public  ArrayList<List<String>> dataktp = new ArrayList<>();
+}
